b3507468ee70c2faaf637159ef8d071862bbe2c4
fengyu19 <853159963@qq.com>
2018-09-10 00:20:52 -0700

    Update config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -5,5 +5,5 @@ class Config:
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = 'yanfengyu0612@gmail.com'
-	MAIL_PASSWORD = 'fengyuxinmima'
+	MAIL_USERNAME = 'xxx'
+	MAIL_PASSWORD = 'xxx'
